09107b65224510e0500092e00d165400ad3389bf
GerryX17 <45268024+GerryX17@users.noreply.github.com>
2018-12-04 13:15:54 +0100

    Add files via upload

--- DrawPanel.java
+++ DrawPanel.java
@@ -1,14 +1,17 @@
 package myguiproject;
 
+import java.awt.event.*;
 import java.awt.*;
+import static java.lang.Boolean.TRUE;
 import javax.swing.*;
 import java.util.*;
 
-public class DrawPanel extends JPanel {
+public class DrawPanel extends JPanel implements MouseListener{
 	protected LinkedList< Entity > drawables;
-
+        protected LinkedList< Entity > selection;
 	public DrawPanel() {
 		drawables = new LinkedList< Entity >();
+                addMouseListener( this );
 	}
 
 	public void addDrawable( Entity entity ) {
@@ -25,4 +28,18 @@ public class DrawPanel extends JPanel {
 		for ( int i = 0; i < drawables.size(); ++i )
 			drawables.get( i ).translate( dx, dy );
 	}
+        @Override
+        public void mousePressed(MouseEvent e){
+        Point c= new Point(e.getX(),e.getY());
+        if (isPointInside(Point c) == TRUE)
+        System.out.println("'The point is inside");
+        }
+        
+        @Override
+        public void mouseReleased(MouseEvent e);
+        public void mouseEntered(MouseEvent e);
+        @Override
+        public void mouseExited(MouseEvent e);
+        @Override
+        public void mouseClicked(MouseEvent e);
 }
