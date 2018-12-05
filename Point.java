7633540895164429c236b7eb211514b92298334f
danisafe3 <37106410+danisafe3@users.noreply.github.com>
2018-12-05 11:22:36 +0100

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,38 @@
+package myguiproject;
+
+public class Point {
+
+    protected int x;
+    protected int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+    
+    
+    public String infoString() {
+        return ("Point (" + x + "," + y + ")");
+    }
+    
+    public void move(int x, int y){
+        this.x += x;
+        this.y += y;
+    }
+    
+    public Vector difference(Point a){
+        int newX = this.x - a.getX();
+        int newY = this.y - a.getY();
+        Vector v = new Vector(newX, newY);
+        return v;
+    }
+
+}
